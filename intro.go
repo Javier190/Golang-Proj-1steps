d576a2dd71de08fd1f7e5bd49ab5f9b993a02028
Javier <jav.aravenac@gmail.com>
2023-08-16 17:16:28 -0400

    functions & conditions

--- intro.go
+++ intro.go
@@ -12,5 +12,46 @@ func main(){
 	var nombre = "Javier"
 
 	fmt.Print("mi nombres es ", nombre , " y tengo " , edad, " años")
+
+
+//Entradas
+	var web string
+	fmt.Println("Introduce una entrada")
+	fmt.Scan(&web)
+	fmt.Println("Tu entrada fue: ", web)
+
+
+//Condicionales
+
+	var altura int
+
+	fmt.Print("¿Cual es tu altura en cms?")
+	fmt.Scan(&altura)
+
+	if altura >= 180 {
+		fmt.Println("Eres Alto ! felicidades")
+	} else {
+		fmt.Println("Eres bajo que pena")
+	}
+
+
+	//Funciones
+
+	fmt.Println("Calculando IMC con peso: ", calcularIMC(2))
+	fmt.Println("------------------------------------------")
+	fmt.Println("Calculando IMC con Altura: ", calcularIMCAltura(altura))
+
 }
 
+func calcularIMC(peso int) int {
+
+	var resultado int
+	resultado = peso * 5
+	return resultado
+}
+
+func calcularIMCAltura(altura int) int {
+	var resultado int
+	resultado = altura * 3
+	return resultado
+}
