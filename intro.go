495c748b8c56e264b4cb594f99dd73f0b79c0378
Javier <jav.aravenac@gmail.com>
2023-08-16 16:31:56 -0400

    Some random Code

---
+++ intro.go
@@ -0,0 +1,16 @@
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Print("Hola Mundo Golang ! ")
+
+
+
+	var edad = 32
+	var nombre = "Javier"
+
+	fmt.Print("mi nombres es ", nombre , " y tengo " , edad, " años")
+}
+
